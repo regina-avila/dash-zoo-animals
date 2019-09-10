ac86361fc7a08619886dc68d29076228035fd896
Austin Lasseter <austinlasseter@gmail.com>
2019-09-10 18:26:44 -0400

    updated everything

---
+++ app.py
@@ -0,0 +1,39 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+########### Pick variables
+tabtitle='zoo'
+myheading = 'Animals in the Zoo'
+myfavoritecolor='#C74A2F' # More colors are here: https://htmlcolorcodes.com/
+x_list=['giraffes', 'elephants', 'monkeys']
+y_list=[5, 2, 7]
+mytitle='My favorite animals'
+githublink='https://github.com/austinlasseter/zoo-animals-dash'
+
+########### Set up the chart
+mydata = [go.Bar(x=x_list,
+                y=y_list,
+                marker=dict(color=myfavoritecolor))]
+mylayout = go.Layout(
+    title = mytitle,
+    xaxis = dict(title = 'Labels go here!'),
+    yaxis = dict(title = 'Numbers go here!'))
+myfigure = go.Figure(data=mydata, layout=mylayout)
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Display the chart
+app.layout = html.Div(children=[
+    html.H1(myheading),
+    dcc.Graph(id='figure-1', figure=myfigure),
+    html.A('Code on Github', href=githublink)])
+
+########### Execute your code
+if __name__ == '__main__':
+    app.run_server()
